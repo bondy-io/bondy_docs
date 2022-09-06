cc1dd3b194302651a17b88c07bc7e678d8832bb7
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2022-09-06 14:29:59 +0100

    added ZoomImg component
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,14 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
 import MyLayout from './MyLayout.vue'
 import DataTreeView from './DataTreeView.vue'
-// import TreeView from "@grapoza/vue-tree"
+import ZoomImg from './ZoomImg.vue'
 
+import './custom.css'
 
 export default {
     ...DefaultTheme,
     Layout: MyLayout,
     enhanceApp({ app }) {
         app.component('DataTreeView', DataTreeView)
+        app.component('ZoomImg', ZoomImg)
     }
 }
