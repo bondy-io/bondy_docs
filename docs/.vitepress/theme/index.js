cb53b98a9a7926ab88fa918a285100bf8d6d23bc
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2022-09-13 17:51:51 +0100

    added tabs
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,6 +3,7 @@ import MyLayout from './MyLayout.vue'
 import DataTreeView from './DataTreeView.vue'
 import ZoomImg from './ZoomImg.vue'
 import Features from './Features.vue'
+import {Tabs, Tab} from 'vue3-tabs-component';
 
 import './custom.css'
 
@@ -10,6 +11,8 @@ export default {
     ...DefaultTheme,
     Layout: MyLayout,
     enhanceApp({ app }) {
+        app.component('Tab', Tab)
+        app.component('Tabs', Tabs)
         app.component('DataTreeView', DataTreeView)
         app.component('ZoomImg', ZoomImg)
         app.component('Features', Features)
