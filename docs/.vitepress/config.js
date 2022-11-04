8daac7e621d91ade4cc91afa2437e9bca1c2b5b2
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2022-11-04 11:15:17 +0000

    added discourse logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -199,7 +199,13 @@ export default {
         socialLinks: [
             { icon: 'github', link: 'https://github.com/bondy-io'},
             { icon: 'twitter', link: 'https://twitter.com/bondyIO' },
-            { icon: 'slack', link: 'https://join.slack.com/t/bondy-group/shared_invite/zt-1j1fbpr04-BUesuqeWBbblbqUPsXrP1A' }
+            { icon: 'slack', link: 'https://join.slack.com/t/bondy-group/shared_invite/zt-1j1fbpr04-BUesuqeWBbblbqUPsXrP1A' },
+            {
+              icon: {
+                svg: '<svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><title>Discourse</title><path d="M12.103 0C18.666 0 24 5.485 24 11.997c0 6.51-5.33 11.99-11.9 11.99L0 24V11.79C0 5.28 5.532 0 12.103 0zm.116 4.563a7.395 7.395 0 0 0-6.337 3.57 7.247 7.247 0 0 0-.148 7.22L4.4 19.61l4.794-1.074a7.424 7.424 0 0 0 8.136-1.39 7.256 7.256 0 0 0 1.737-7.997 7.375 7.375 0 0 0-6.84-4.585h-.008z"/></svg>'
+              },
+              link: 'https://join.slack.com/t/bondy-group/shared_invite/zt-1j1fbpr04-BUesuqeWBbblbqUPsXrP1A'
+            }
         ],
 
         // The footer will displayed only when the page doesn't contain sidebar
