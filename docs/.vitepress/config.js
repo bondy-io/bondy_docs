4005228aa5197e4228a24c97180c7096bb186441
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2022-12-09 11:58:58 +0000

    added favicons
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,7 +12,14 @@ export default {
       ['meta', { property: 'description', content: 'Learn how to develop, deploy and manage distributed applications using Bondy. Bondy is an open source, always-on and scalable application networking platform connecting all elements of a distributed application—offering event and service mesh capabilities combined. From web and mobile apps to IoT devices and backend microservices, Bondy allows everything to talk using one simple communication protocol.' }],
       ['meta', { property: 'og:description', content: 'Learn how to develop, deploy and manage distributed applications using Bondy. Bondy is an open source, always-on and scalable application networking platform connecting all elements of a distributed application—offering event and service mesh capabilities combined. From web and mobile apps to IoT devices and backend microservices, Bondy allows everything to talk using one simple communication protocol.' }],
       ['meta', { property: 'keywords', content: "distributed application, application networking platform, scalable, always-on, universal protocol, remote procedure call, RPC, service mesh, publish-subscribe, publish/subscribe, event mesh, authorization, authentication, web application messaging protocol, router, WAMP, wamp router, API gateway, kubernetes, microservices, p2p, erlang"
-        }]
+        }],
+      ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml"}],
+      ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
+      ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
+      ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+      ['link', { rel: "shortcut icon", href: "/assets/favicons/favicon.ico"}]
     ],
 
     appearance: true,
